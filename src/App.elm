6a95ddb8c3f12dcf1cca62bd4b7f716ab200d379
Derek Cuevas <dccuevas2@gmail.com>
2017-03-12 14:58:24 -0700

    creating view functions

--- src/App.elm
+++ src/App.elm
@@ -1,30 +1,95 @@
 module App exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- MODEL
+
+
+mockFriends : List Friend
+mockFriends =
+    [ (Friend 0 "john" "@jj")
+    , (Friend 1 "santa" "@s")
+    , (Friend 2 "steve" "@apple")
+    ]
+
+
+type alias Friend =
+    { id : Int
+    , name : String
+    , username : String
+    }
 
 
 type alias Model =
-    { message : String
+    { query : String
+    , friends : List Friend
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "Hello world!", Cmd.none )
+    ( Model "" mockFriends, Cmd.none )
+
+
+
+-- UPDATE
 
 
 type Msg
-    = NoOp
+    = SetQuery String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SetQuery newQuery ->
+            ( { model | query = newQuery }, Cmd.none )
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    text model.message
+    div [ class "app" ]
+        [ (viewSearchInput model.query)
+        , (viewFriendList model.friends)
+        ]
+
+
+viewSearchInput : String -> Html Msg
+viewSearchInput query =
+    input
+        [ type_ "search"
+        , placeholder "Search friends..."
+        , autocomplete False
+        , class "search-input"
+        , onInput SetQuery
+        ]
+        [ text query ]
+
+
+viewFriendList : List Friend -> Html Msg
+viewFriendList friends =
+    ul [ class "friend-list" ] (List.map viewFriend friends)
+
+
+viewFriend : Friend -> Html Msg
+viewFriend friend =
+    li []
+        [ h4 [ class "friend-thumbnail" ]
+            [ text (friend.name ++ " ")
+            , span [ class "username" ] [ text friend.username ]
+            ]
+        ]
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
